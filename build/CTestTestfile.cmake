dcc4d915ceba0a69e6ef31c200fe06f9b2e8205a
cqwuxiaolong <cqwuxiaolong@qq.com>
2021-02-15 00:50:12 +0800

    master

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/cqwuxiaolong/HelloOcct
+# Build directory: C:/Users/cqwuxiaolong/HelloOcct/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
